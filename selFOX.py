2dcbee81616b20875d4c574c300fddff2a075f3f
Nikhil Karnik <nikhil@zillabyte.com>
2015-01-07 15:10:00 -0800

    working selenium fte

--- selFOX.py
+++ selFOX.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
-base_facebook = 
+base_facebook = "http://graph.facebook.com"
 
 def fte_fetch_comments(url):
 
@@ -23,3 +23,25 @@ def fte_fetch_comments(url):
   driver.switch_to.frame(fid)
   
   comments = driver.find_elements_by_class_name("postText")
+  commenters = driver.find_elements_by_class_name("postActor")
+
+  morec = driver.find_element_by_class_name("pam")
+  morec.click()
+
+  
+  comments = driver.find_elements_by_class_name("postText")
+  commenters = driver.find_elements_by_class_name("postActor")
+ 
+  return comments, commenters
+
+def print_coms(comments, commenters):
+
+  for com in comments:
+    print com.text
+
+  for com in commenters:
+    print com.get_attribute("href")
+
+c, cs = fte_fetch_comments("http://fivethirtyeight.com/datalab/chris-christie-2016-president-republican-primary-overrated/")
+
+print_coms(c, cs) 
